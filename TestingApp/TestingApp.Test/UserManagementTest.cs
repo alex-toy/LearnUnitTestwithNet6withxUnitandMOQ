0f1e5fe509a971baffaf055a87c7834be7f1853c
alex-toy <alexei.80@hotmail.fr>
2023-06-11 19:47:48 +0200

    added tests for user

--- TestingApp/TestingApp.Test/UserManagementTest.cs
+++ TestingApp/TestingApp.Test/UserManagementTest.cs
@@ -1,13 +1,37 @@
+using System.Linq;
+using TestingApp.Functionality;
+using TestingApp.Models;
 using Xunit;
 
 namespace TestingApp.Test
-{
-    public class UnitTest1
+{   
+    public class UserManagementTest
     {
         [Fact]
-        public void Test1()
+        public void Add_create_user()
         {
+            var userManagement = new UserManagement();
+            userManagement.Add(new User("alex", "BG") { });
+            var savedUser = Assert.Single(userManagement.AllUsers);
+            Assert.NotNull(savedUser);
+            Assert.Equal("alex", savedUser.firstName);
+            Assert.Equal("BG", savedUser.lastName);
+            Assert.False(savedUser.VerifiedEmail);
+        }
 
+        [Fact]
+        public void Update_mobile_number()
+        {
+            var userManagement = new UserManagement();
+            userManagement.Add(new User("alex", "BG") { });
+            var firstUser = userManagement.AllUsers.First();
+            firstUser.Phone = "+03387654";
+
+            userManagement.UpdatePhone(firstUser);
+
+            var savedUser = Assert.Single(userManagement.AllUsers);
+            Assert.NotNull(savedUser);
+            Assert.Equal("+03387654", savedUser.Phone);
         }
     }
 }
